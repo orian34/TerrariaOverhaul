d04398f8551d348ce98dc88483e0fe615fa3b14e
Mirsario <me@mirsar.io>
2024-03-31 22:56:26 +0300

    Updated localization stats task to support split files.

--- BuildTools/GenerateLocalizationStats.cs
+++ BuildTools/GenerateLocalizationStats.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Transactions;
 using Hjson;
 using Microsoft.Build.Framework;
 using Newtonsoft.Json;
@@ -15,89 +18,121 @@ namespace TerrariaOverhaul.BuildTools;
 /// </summary>
 public sealed class GenerateLocalizationStats : TaskBase
 {
-	private class RecursionData
+	private class Stats
 	{
-		public JObject Translation { get; }
 		public int PresentTranslationCount { get; set; }
 		public int MissingTranslationCount { get; set; }
 		
 		public int TotalTranslationCount => PresentTranslationCount + MissingTranslationCount;
-
-		public RecursionData(JObject translation)
-		{
-			Translation = translation;
-		}
 	}
+
+	private static readonly Regex culturePrefixRegex = new(@"([a-z][a-z]-[A-Za-z]+)[\w/\\]*?(?:_?((?:\w+\.?)+))?\.hjson");
+	private static readonly string[] knownCultures = {
+		"de-DE", "es-ES", "fr-FR", "it-IT", "pl-PL", "pt-BR", "ru-RU", "zh-Hans",
+	};
 	
 	[Required]
 	public string ResultsOutputPath { get; set; } = string.Empty;
 
 	[Required]
-	public string MainFile { get; set; } = string.Empty;
+	public string MainCulture { get; set; } = string.Empty;
 
 	[Required]
 	public string[] LocalizationFiles { get; set; } = Array.Empty<string>();
 
 	protected override void Run()
 	{
-		var baseTranslation = ReadHjsonFile(MainFile);
-		var results = new Dictionary<string, RecursionData>();
+		var localizationsByCulture = new Dictionary<string, JObject>();
 
-		foreach (string translationFilePath in LocalizationFiles) {
-			string cultureName = Path.GetFileNameWithoutExtension(translationFilePath);
-
-			var translation = ReadHjsonFile(translationFilePath);
-			var data = new RecursionData(translation);
-
-			Recursion(data, baseTranslation);
-
-			results[cultureName] = data;
-		}
-
-		if (!string.IsNullOrWhiteSpace(ResultsOutputPath)) {
-			string usedPath = Path.ChangeExtension(ResultsOutputPath, ".md");
-			var resultsText = new StringBuilder();
-
-			const string Header = "# Results of the last localization refresh";
-
-			// Prevent commas from being used in place of periods.
-			// We don't want that to appear in PRs.
-			CultureInfo.CurrentCulture = new CultureInfo("en-US");
-
-			resultsText.AppendLine(Header);
-			resultsText.AppendLine();
-
-			foreach (var pair in results) {
-				string cultureName = pair.Key;
-				var data = pair.Value;
-
-				string status = data.PresentTranslationCount != 0 ? (data.MissingTranslationCount == 0 ? "✅ Full!" : "⚠️ Incomplete!") : "❌ Not even started!";
-
-				resultsText.AppendLine($"## {cultureName}");
-				resultsText.AppendLine($"- **Status:** {status}");
-				resultsText.AppendLine($"- **Completion:** ***{data.PresentTranslationCount / (float)data.TotalTranslationCount * 100f:0.0}%***");
-				resultsText.AppendLine($"- **Translated:** `{data.PresentTranslationCount}` out of `{data.TotalTranslationCount}` (`{data.MissingTranslationCount}` missing!)");
-				resultsText.AppendLine();
+		foreach (string path in LocalizationFiles) {
+			if (!TryGetCultureAndPrefixFromPath(path, out string cultureName, out string prefix)) {
+				throw new InvalidOperationException($"Path not parsed: {path}");
 			}
 
-			string finalizedResultsText = resultsText.ToString();
+			var translation = ReadHjsonFile(path);
 
-			if (File.Exists(usedPath)) {
-				string existingText = File.ReadAllText(usedPath);
-				int headerIndex = existingText.IndexOf(Header);
+			if (!string.IsNullOrWhiteSpace(prefix)) {
+				string[] keys = prefix.Split('.');
 
-				if (headerIndex >= 0) {
-					finalizedResultsText = $"{existingText.Substring(0, headerIndex)}{finalizedResultsText}";
+				for (int i = keys.Length - 1; i >= 0; i--) {
+					translation = new JObject {
+						[keys[i]] = translation
+					};
 				}
 			}
 
-			File.WriteAllText(usedPath, finalizedResultsText);
+			if (!localizationsByCulture.TryGetValue(cultureName, out var json)) {
+				localizationsByCulture[cultureName] = translation;
+			} else {
+				json.Merge(translation);
+			}
 		}
+
+		if (!localizationsByCulture.TryGetValue(MainCulture, out var baseLocalization)) {
+			throw new InvalidOperationException($"No main culture files found!\r\nAll keys: {string.Join(", ", localizationsByCulture.Keys)}");
+		}
+
+		var results = new Dictionary<string, Stats>();
+
+		foreach (var pair in localizationsByCulture) {
+			if (!results.TryGetValue(pair.Key, out var stats)) {
+				results[pair.Key] = stats = new Stats();
+			}
+
+			Recursion(pair.Value, stats, baseLocalization);
+		}
+
+		WriteResults(results);
 	}
 
-	private static JObject ReadHjsonFile(string fileName)
+	private void WriteResults(Dictionary<string, Stats> results)
 	{
-		string hjsonText = File.ReadAllText(fileName);
+		if (string.IsNullOrWhiteSpace(ResultsOutputPath)) {
+			return;
+		}
+
+		string usedPath = Path.ChangeExtension(ResultsOutputPath, ".md");
+		var resultsText = new StringBuilder();
+
+		const string Header = "# Results of the last localization refresh";
+
+		// Prevent commas from being used in place of periods.
+		// We don't want that to appear in PRs.
+		CultureInfo.CurrentCulture = new CultureInfo("en-US");
+
+		resultsText.AppendLine(Header);
+		resultsText.AppendLine();
+
+		foreach (var pair in results) {
+			string cultureName = pair.Key;
+			var data = pair.Value;
+
+			string status = data.PresentTranslationCount != 0 ? (data.MissingTranslationCount == 0 ? "✅ Full!" : "⚠️ Incomplete!") : "❌ Not even started!";
+
+			resultsText.AppendLine($"## {cultureName}");
+			resultsText.AppendLine($"- **Status:** {status}");
+			resultsText.AppendLine($"- **Completion:** ***{data.PresentTranslationCount / (float)data.TotalTranslationCount * 100f:0.0}%***");
+			resultsText.AppendLine($"- **Translated:** `{data.PresentTranslationCount}` out of `{data.TotalTranslationCount}` (`{data.MissingTranslationCount}` missing!)");
+			resultsText.AppendLine();
+		}
+
+		string finalizedResultsText = resultsText.ToString();
+
+		if (File.Exists(usedPath)) {
+			string existingText = File.ReadAllText(usedPath);
+			int headerIndex = existingText.IndexOf(Header);
+
+			if (headerIndex >= 0) {
+				finalizedResultsText = $"{existingText.Substring(0, headerIndex)}{finalizedResultsText}";
+			}
+		}
+
+		File.WriteAllText(usedPath, finalizedResultsText);
+	}
+
+	private static JObject ReadHjsonFile(string filePath)
+	{
+		string hjsonText = File.ReadAllText(filePath);
 		using var hjsonReader = new StringReader(hjsonText);
 
 		string jsonText = HjsonValue.Load(hjsonReader).ToString();
@@ -106,31 +141,45 @@ public sealed class GenerateLocalizationStats : TaskBase
 		return jsonObject;
 	}
 
-	private static void Recursion(RecursionData data, JToken token)
+	private static void Recursion(JObject translation, Stats stats, JToken token)
 	{
-		var translation = data.Translation;
 		var translatedToken = translation.SelectToken(token.Path);
 
 		switch (token) {
 			case JProperty jsonProperty:
 				if (jsonProperty.Value.Type != JTokenType.Object) {
 					if (translatedToken == null) {
-						data.MissingTranslationCount++;
+						stats.MissingTranslationCount++;
 					} else {
-						data.PresentTranslationCount++;
+						stats.PresentTranslationCount++;
 					}
 				}
 
-				Recursion(data, jsonProperty.Value);
+				Recursion(translation, stats, jsonProperty.Value);
 				break;
 			case JObject jsonObject:
 				var properties = jsonObject.Properties();
 
 				foreach (var property in properties) {
-					Recursion(data, property);
+					Recursion(translation, stats, property);
 				}
 
 				break;
 		}
+	}
+
+	private static bool TryGetCultureAndPrefixFromPath(string path, out string culture, out string prefix)
+	{
+		var match = culturePrefixRegex.Match(path);
+
+		if (match.Success) {
+			culture = match.Groups[1].Value;
+			prefix = match.Groups[2].Value;
+			return true;
+		}
+
+		culture = default!;
+		prefix = default!;
+		return false;
 	}
 }
